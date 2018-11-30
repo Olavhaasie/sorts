6029c2b15dca3036cf0568c613e2159d4fdbf085
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2018-11-30 20:06:51 +0100

    add benchmarks

--- benches/merge_sort.rs
+++ benches/merge_sort.rs
@@ -4,14 +4,14 @@ extern crate sorts;
 extern crate test;
 
 use rand::prelude::*;
-use sorts::bubble_sort;
+use sorts::merge_sort;
 use test::Bencher;
 
 #[bench]
 fn bench_sort_small(b: &mut Bencher) {
     let test = vec![6, 7, 3, 4, 5, 1, 8, 9, 2];
     b.iter(|| {
-        bubble_sort(&mut test.clone());
+        merge_sort(&mut test.clone());
     });
 }
 
@@ -20,6 +20,6 @@ fn bench_sort_big(b: &mut Bencher) {
     let mut test: Vec<_> = (0..10_000).collect();
     rand::thread_rng().shuffle(&mut test);
     b.iter(|| {
-        bubble_sort(&mut test.clone());
+        merge_sort(&mut test.clone());
     });
 }
