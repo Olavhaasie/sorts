b8270b1cf22579176630ce71c6ebcdf58ce5a9ea
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2018-11-30 20:46:51 +0100

    implement insertion sort

---
+++ src/insertion_sort.rs
@@ -0,0 +1,43 @@
+/// Sorts a slice in-place using
+/// [insertion sort](https://en.wikipedia.org/wiki/Insertion_sort).
+///
+/// This sorting algorithm is very efficient when used on small data sets.
+/// This is because insertion sort has constant space complexity and works
+/// very fast when used on partially sorted data.
+///
+/// # Examples
+/// ```
+/// let mut vec = vec![-4, -5, 7, 45, 0];
+/// sorts::insertion_sort(&mut vec);
+/// assert_eq!(vec, &[-5, -4, 0, 7, 45]);
+/// ```
+pub fn insertion_sort<T: PartialOrd>(s: &mut [T]) {
+    for i in 1..s.len() {
+        let mut j = i;
+        while j > 0 && s[j - 1] > s[j] {
+            s.swap(j - 1, j);
+            j -= 1;
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_sort() {
+        let test = &mut [9,2,5,1,3,4,6,8,7];
+        insertion_sort(test);
+        assert_eq!(test, &[1,2,3,4,5,6,7,8,9]);
+    }
+
+    #[test]
+    fn sort_empty_set() {
+        let test: &mut [u8] = &mut [];
+        insertion_sort(test);
+        assert_eq!(test, &[]);
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 //! This is the documentation for `sorts`. This crate contains a
 //! collection of various sorting algorithms.
 pub mod bubble_sort;
+pub mod insertion_sort;
 pub mod merge_sort;
 
 pub use bubble_sort::bubble_sort;
+pub use insertion_sort::insertion_sort;
 pub use merge_sort::merge_sort;
