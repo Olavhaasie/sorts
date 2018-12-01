07774c8a2466622991320969b5d598fc09db60e9
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2018-12-01 17:26:24 +0100

    add generated integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,15 +6,34 @@ macro_rules! sort_test {
 
         #[test]
         fn $test() {
-            let vec = &mut $input;
+            let vec: &mut [usize] = &mut $input;
             sorts::$sort(vec);
             assert_eq!(vec, &$expected);
         }
     }
 }
 
-#[cfg(test)]
-mod gnome_sort {
-    sort_test!(correct_sort, gnome_sort, [2, 4, 3, 1], [1, 2, 3, 4]);
+macro_rules! sort_tests {
+    ($($sort:ident),*) => {
+        $(
+        #[cfg(test)]
+        mod $sort {
+            sort_test!(sort_even, $sort, [2, 4, 3, 1], [1, 2, 3, 4]);
+            sort_test!(sort_odd, $sort, [2, 4, 3, 1, 0], [0, 1, 2, 3, 4]);
+            sort_test!(sort_sorted, $sort, [3, 4, 5, 6, 7], [3, 4, 5, 6, 7]);
+            sort_test!(sort_reversed, $sort, [9, 8, 7, 6, 5], [5, 6, 7, 8, 9]);
+            sort_test!(sort_empty, $sort, [], []);
+            sort_test!(sort_one, $sort, [100], [100]);
+        }
+        )*
+    }
 }
 
+sort_tests! {
+    bubble_sort,
+    cocktail_sort,
+    gnome_sort,
+    insertion_sort,
+    selection_sort,
+    merge_sort
+}
